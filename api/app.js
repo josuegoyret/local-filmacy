f1c71dff7b0eb7de59b40cea61dfe5f235a27651
josuegoyret <fjgoyretsola@gmail.com>
2024-03-08 11:01:59 -0300

    feat(upload-video): start server and db, and add upload video and thumbnail services

--- api/app.js
+++ api/app.js
@@ -1,1 +1,69 @@
 const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const dotenv = require("dotenv");
+const ffmpeg = require("fluent-ffmpeg");
+const { addVideo } = require("./db");
+const { getVideoDurationInSeconds } = require("get-video-duration");
+const { execSync } = require("child_process");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3001;
+
+const storage = multer.diskStorage({
+  destination: (_req, _file, cb) => {
+    cb(null, "uploads/videos/");
+  },
+  filename: (_req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+const app = express();
+
+const storeVideo = async (file) => {
+  const duration = await getVideoDurationInSeconds(`./uploads/videos/${file}`);
+  addVideo(file, duration);
+};
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
+
+const uploadThumbnail = (originalname) => {
+  ffmpeg(`./uploads/videos/${originalname}`)
+    .screenshots({
+      count: 1,
+      timemarks: ["00:00:03"],
+      folder: "./uploads/thumbnails/",
+      filename: `${originalname.split(".")[0]}.jpg`,
+    })
+    .on("end", () => {
+      console.log("Thumbnail generated");
+    })
+    .on("error", (err) => {
+      console.log(err);
+    });
+};
+
+app.post("/upload-video", upload.single("videofile"), async (req, res) => {
+  try {
+    uploadThumbnail(req.file.originalname);
+    await storeVideo(req.file.originalname);
+    return res.status(200).send({
+      message: "Successfully uploaded video",
+      video: req.file.originalname,
+    });
+  } catch (error) {
+    return res
+      .status(error.status || 500)
+      .send({ message: error.message || "Error while uploading video" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
---
+++ api/db.js
@@ -0,0 +1,37 @@
+const sqlite3 = require("better-sqlite3"); // SQLite3 library for Node.js
+
+const DB_SOURCE = "db.sqlite";
+
+const db = new sqlite3(DB_SOURCE);
+
+try {
+  db.prepare(
+    `
+    CREATE TABLE IF NOT EXISTS videofile (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      filename text UNIQUE,
+      duration text,
+      isFavorite integer,
+      isDeleted integer
+    )
+  `
+  ).run();
+} catch (error) {
+  console.log(
+    "An error occurred while creating the table:",
+    error.message || error
+  );
+}
+
+const addVideo = (filename, duration) => {
+  const statement = db.prepare(`
+    INSERT OR IGNORE INTO videofile (filename, duration, isFavorite, isDeleted) VALUES (?, ?, 0, 0)
+  `);
+  const info = statement.run(filename, duration, 0, 0);
+  return info.changes;
+};
+
+module.exports = {
+  db,
+  addVideo,
+};
