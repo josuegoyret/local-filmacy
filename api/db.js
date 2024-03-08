514d19288ea3641823ca8d8968b128e9c3fcbbae
josuegoyret <fjgoyretsola@gmail.com>
2024-03-08 11:45:35 -0300

    fix: add video to db command

--- api/db.js
+++ api/db.js
@@ -6,27 +6,25 @@ const db = new sqlite3(DB_SOURCE);
 
 try {
   db.prepare(
-    `
-    CREATE TABLE IF NOT EXISTS videofile (
+    `CREATE TABLE IF NOT EXISTS videofile (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       filename text UNIQUE,
       duration text,
       isFavorite integer,
       isDeleted integer
-    )
-  `
+    )`
   ).run();
 } catch (error) {
   console.log(
-    "An error occurred while creating the table:",
+    "An error occurred while creating the table: ",
     error.message || error
   );
 }
 
 const addVideo = (filename, duration) => {
-  const statement = db.prepare(`
-    INSERT OR IGNORE INTO videofile (filename, duration, isFavorite, isDeleted) VALUES (?, ?, 0, 0)
-  `);
+  const statement = db.prepare(
+    `INSERT OR IGNORE INTO videofile (filename, duration, isFavorite, isDeleted) VALUES (?, ?, ?, ?)`
+  );
   const info = statement.run(filename, duration, 0, 0);
   return info.changes;
 };
