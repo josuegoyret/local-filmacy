6a068a6b6a10a1ebd2f014d342aa297605f750a1
josuegoyret <fjgoyretsola@gmail.com>
2024-03-11 16:55:37 -0300

    feat(client/video-card): style and make it responsive

--- client/src/components/VideoCard.vue
+++ client/src/components/VideoCard.vue
@@ -1,19 +1,27 @@
 <template>
-  <div class="flex w-full aspect-video md:aspect-2/3 rounded-md relative shadow-md shadow-black/60">
-    <div class="absolute w-full h-full bg-gradient-to-t from-black/10 to-white/10 rounded-md">
-      <img
-        :src="thumbnailURL"
+  <div
+    class="w-full aspect-video md:aspect-2/3 rounded-md relative border border-neutral-dark-500 hover:border-neutral-medium-500 hover:shadow hover:shadow-neutral-medium-500 transition-all overflow-hidden cursor-pointer"
+  >
+    <img :src="thumbnailURL" class="h-full w-full object-cover" alt="video thumbnail" />
+    <div
+      class="w-full h-full opacity-0 hover:opacity-100 flex flex-col justify-center items-center gap-4 absolute inset-0 hover:bg-black/50 transition-all"
+    >
+      <IconPlay
         @click="playVideo"
-        class="h-full w-full rounded-md z-0 object-cover cursor-pointer"
-        alt="video thumbnail"
+        class="w-10 h-10 md:w-12 md:h-12 hover:scale-105 transition-transform text-display-500"
       />
-      <div class="flex gap-4 absolute bottom-4 right-4 z-20 w-full px-4">
-        <icon-trash
-          class="cursor-pointer hover:scale-125 transition-transform text-white shadow-white"
-        />
-        <icon-fav
-          class="cursor-pointer hover:scale-125 transition-transform text-white shadow-white"
-        />
+      <div class="w-full flex justify-between items-center p-4 absolute z-20 bottom-0">
+        <p class="text-sm text-display-500">{{ props.video.filename }}</p>
+        <div class="flex gap-2 items-center">
+          <IconFav
+            @click="favVideo"
+            class="cursor-pointer hover:scale-105 transition-transform hover:text-display-500"
+          />
+          <IconTrash
+            @click="deleteVideo"
+            class="cursor-pointer hover:scale-105 transition-transform hover:text-display-500"
+          />
+        </div>
       </div>
     </div>
   </div>
@@ -21,11 +29,12 @@
 
 <script setup lang="ts">
 import type { Video } from '@/common/types'
-import IconFav from './icons/IconFav.vue'
-import IconTrash from './icons/IconTrash.vue'
 import { computed } from 'vue'
 import { getThumbnailURL } from '@/utils/videos.util'
 import { useVideosStore } from '@/stores/videos'
+import IconFav from './icons/IconFav.vue'
+import IconPlay from './icons/IconPlay.vue'
+import IconTrash from './icons/IconTrash.vue'
 
 interface VideoCardProps {
   video: Video
@@ -38,5 +47,11 @@ const thumbnailURL = computed(getThumbnailURL(props.video.filename))
 const playVideo = () => {
   videosStore.selectedVideo = props.video.filename
   videosStore.isVideoPlayerVisible = true
+}
+const favVideo = () => {
+  console.log('fav video')
+}
+const deleteVideo = () => {
+  console.log('delete video')
 }
 </script>
